127562a162bea31f491f1934e9bb24f7c0139232
TheEinfachFelix <the.einfach.felix@gmail.com>
2022-10-11 01:23:40 +0200

    testing

--- Light_sensor_test/Light_sensor_test.ino
+++ Light_sensor_test/Light_sensor_test.ino
@@ -1,9 +1,10 @@
 const int LightSensor = 16;
 const int WLightSensor = 13;
-char d= 'A0';
+int d = A0;
 
 void setup() {
   // put your setup code here, to run once:
+  //pinMode(d, INPUT);
   Serial.begin(9600);
 }
 
