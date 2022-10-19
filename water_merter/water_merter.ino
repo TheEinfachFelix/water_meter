2cd045377bcaf85ecb388d9d28edee6178941848
TheEinfachFelix <the.einfach.felix@gmail.com>
2022-10-19 20:46:15 +0200

    code edit from school

--- water_merter/water_merter.ino
+++ water_merter/water_merter.ino
@@ -6,9 +6,9 @@
 #include <Adafruit_ADS1X15.h>
  
 RTC_DS3231 rtc;
-Adafruit_ADS1115 ads;
+//Adafruit_ADS1115 ads;
 
-// pin out
+//------------pin out------------
 const uint8_t input1 = 12;
 const uint8_t LSensor = 2;
 const uint8_t WSensor = 0;
@@ -16,24 +16,24 @@ const uint8_t readLED = 15;
 const uint8_t testLED = 13;
 const uint8_t enable = 12;
 
-//adc pin out
+//------------adc pin out------------
 const uint8_t PhotoR1 = 0;
 const uint8_t PhotoR2 = 1;
 const uint8_t HallSenFlow = 2;
 const uint8_t HallSenDepth = 3;
 
-//eeprom vars
+//------------eeprom vars------------
 uint16_t eepromAdres = 2;
-uint8_t packageSize[] = {4,2,2,2,2,2,2};
+uint8_t packageSize[] = {4,2,2,2,2,2,2}; //16 byte in total
 bool live = 0;
 
-//deepSleep
-uint32_t deepSleep = (1*60*1000000);
+//------------deepSleep------------
+uint32_t sleepTime = (1*60*1000000);
 
-// gets the length of an array
+//------------gets the length of an array------------
 #define ELEMENTCOUNT(x)  (sizeof(x) / sizeof(x[0]))
 
-//temperatur sensor stuff
+//------------temperatur sensor stuff------------
 OneWire LoneWire(LSensor);
 OneWire WoneWire(WSensor);
 DallasTemperature Lsensors(&LoneWire);
@@ -43,9 +43,10 @@ DallasTemperature Wsensors(&WoneWire);
 void setup() {  
   Serial.begin(115200);
   EEPROM.begin(4096);
-  delay(600);
+  delay(800);
   Serial.println("wake up");
   
+  //------------for RTC------------
   #ifndef ESP8266 //for RTC
   while (!Serial); // for Leonardo/Micro/Zero
 #endif
@@ -56,14 +57,13 @@ void setup() {
   if (rtc.lostPower()) { //power loss reset 
     Serial.println("RTC lost power, lets set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  } // end of RTC 
+  } 
+  //------------end of RTC------------ 
   
-  lasthit =  millis();
-
-  //temp sensor
+  //------------temp sensor------------
   Lsensors.begin();
   Wsensors.begin();
-  //adc stuff
+  //------------adc stuff------------
   ads.setGain(GAIN_TWOTHIRDS);
   ads.begin();
 
@@ -74,7 +74,7 @@ void setup() {
   //eepromClear();
   Serial.println("OK lets Go!");
     
-////////////////////////////////////////////////////
+//------------loops stuff for deepSleep------------
   digitalWrite(readLED, 1);
   eepromWrite();
   SerialResponse();
@@ -104,16 +104,16 @@ void loop() {
 //--------------------------------------------Sensor------------------------------------------------
 uint16_t Ltemp(){
   Lsensors.requestTemperatures();
-  float LtemperatureC = Lsensors.getTempCByIndex(0);
-  LtemperatureC = LtemperatureC*100;
-  return(LtemperatureC);
+  float LtempeC = Lsensors.getTempCByIndex(0);
+  LtempeC = LtempeC*100;
+  return(LtempeC);
 }
 
 uint16_t Wtemp(){
   Wsensors.requestTemperatures();
-  float WtemperatureC = Wsensors.getTempCByIndex(0);
-  WtemperatureC = WtemperatureC*100;
-  return(WtemperatureC);
+  float WtempeC = Wsensors.getTempCByIndex(0);
+  WtempeC = WtempeC*100;
+  return(WtempeC);
 }
 
 uint16_t adsRead(int p){
@@ -146,25 +146,25 @@ void eepromPrint(){
     Serial.print(" Ltemp: "+String(m3));
     a += packageSize[2];
     //ads
-    //sen1
+    //PhotoR1
     uint16_t m4;     
     EEPROM.get(a, m4);
-    Serial.print(" Sen1: "+String(m4));
+    Serial.print(" PhotoR1: "+String(m4));
     a += packageSize[3];
-    //sen2
+    //PhotoR2
     uint16_t m5;     
     EEPROM.get(a, m5);
-    Serial.print(" Sen2: "+String(m5));
+    Serial.print(" PhotoR2: "+String(m5));
     a += packageSize[4];
-    //sen3
-    uint16_t m5;     
-    EEPROM.get(a, m5);
-    Serial.print(" Sen3: "+String(m5));
-    a += packageSize[5];
-    //sen4 
-        uint16_t m6;     
+    //HallSenFlow
+    uint16_t m6;     
     EEPROM.get(a, m6);
-    Serial.print(" Sen1: "+String(m6));
+    Serial.print(" HallSenFlow: "+String(m6));
+    a += packageSize[5];
+    //HallSenDepth 
+    uint16_t m7;     
+    EEPROM.get(a, m7);
+    Serial.print(" HallSenDepth: "+String(m7));
     a += packageSize[6];
 
     
@@ -193,12 +193,16 @@ void eepromWrite(){
   EEPROM.put(eepromAdres,Ltemp());
   eepromAdres += packageSize[2];
   //adc
+  //PhotoR1
   EEPROM.put(eepromAdres,adsRead(PhotoR1));
   eepromAdres += packageSize[3];
+  //PhotoR2
   EEPROM.put(eepromAdres,adsRead(PhotoR2));
   eepromAdres += packageSize[4];
+  //HallSenFlow
   EEPROM.put(eepromAdres,adsRead(HallSenFlow));
   eepromAdres += packageSize[5];
+  //HallSenDepth
   EEPROM.put(eepromAdres,adsRead(HallSenDepth));
   eepromAdres += packageSize[6];
 
@@ -218,9 +222,15 @@ void SerialResponse(){
     Serial.println("cleared!");
   }
   else if (input == "live"){
-    live = 1;
-    Serial.println("Going Live");
-  }
+    if (live){
+      live = 0;
+      Serial.println("Live on");  
+    } else {
+      live = 1;
+      Serial.println("Going Live");
+
+    }
+      }
   else if (input == "q"){
   Serial.println("quit");
   }
